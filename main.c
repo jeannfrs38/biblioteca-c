cd8cd34771e23d5762a130588d61dc3d78a71af9
jeannfrs <jeanfrs38@gmail.com>
2025-11-25 20:23:06 -0300

    Feat - Add Cadastra and GetAll and GetFirst livros

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+//enum status
+enum Status
+{
+    Disponivel = 0 ,
+    Indisponivel = 1
+};
 // struct como nao temos uma biblioteca para trabalhar com datas criei uma struct
 struct Data
 {
@@ -18,11 +25,9 @@ struct Book
     char Editora[80];
     int AnoPublicacao;
     int QuantidadeDisponivel;
-    bool Status;
+    enum Status StatusEmprestimo;
 };
-
 // um tipo Usuario para facilitar o cadastro de usuario
-
 struct User
 {
     int  Matricula;
@@ -31,7 +36,6 @@ struct User
     char Telefone[15];
     struct Data DataCadastro;
 };
-
 //Struct do tipo emprestimo para facilitar o emprestimo de um livro
 struct  Loan
 {
@@ -43,44 +47,57 @@ struct  Loan
     bool Status;
 };
 
-void MenuLivro(void);
+
+
+// Iniciando Funcoes dos sub-menus
+void MenuLivro(struct Book books[], int *totalLivros);
 void MenuUsuario(void);
 void MenuEmprestimo(void);
 // Funcao Menu principal
 void MenuPrincipal(void){
+    struct Book books[100];
+    struct User users[100];
+    struct Loan Loan[100];
+    int totalLivros = 0;
     int opcao;
-    system("clear");
-    printf("|-------------------- Biblioteca --------------------|\n");
-    printf("\n");
-    printf("|1 - Livro\n");
-    printf("|2 - Usuarios\n");
-    printf("|3 - Emprestimo\n");
-    printf("|4 - Sair\n");
-    printf("\n");
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
+    do {
+        system("clear");
+        printf("|-------------------- Biblioteca --------------------|\n");
+        printf("\n");
+        printf("|1 - Livro\n");
+        printf("|2 - Usuarios\n");
+        printf("|3 - Emprestimo\n");
+        printf("|4 - Sair\n");
+        printf("\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-    switch (opcao) {
-        case 1:
-            MenuLivro();
-            break;
-        case 2:
-            MenuUsuario();
-            break;
-        case 3:
-            MenuEmprestimo();
-            break;
-        case 4:
-        default:
-            system("clear");
-            printf("Opcao Invalida");
-            MenuPrincipal();
-            break;
-    };
+        switch (opcao) {
+            case 1:
+                MenuLivro(books, &totalLivros);
+                break;
+            case 2:
+                MenuUsuario();
+                break;
+            case 3:
+                MenuEmprestimo();
+                break;
+            case 4:
+            default:
+                system("clear");
+                printf("Opcao Invalida");
+                break;
+        };
+    }while (opcao != 4);
+
 };
 
-// Funcao Livro;
-void MenuLivro()
+// Iniciando Funcoes do sub-menu Livros.
+void CadastraLivro(struct Book books[], int *totalLivros);
+void GetFirstBook(struct Book books[], int *totalLivros);
+void GetAllBooks(struct Book books[], int *totalLivros);
+// Funcoes MenuLivro
+void MenuLivro(struct Book books[], int *totalLivros)
 {
     int opcao;
     system("clear");
@@ -96,17 +113,103 @@ void MenuLivro()
     switch (opcao)
     {
         case 1:
+            CadastraLivro(books, totalLivros);
             break;
         case 2:
+            GetFirstBook(books, totalLivros);
             break;
         case 3:
+            GetAllBooks(books, totalLivros);
             break;
         case 4:
         default:
-            MenuPrincipal();
+            return;
             break;
     }
 }
+void CadastraLivro(struct Book books[], int *totalLivros)
+{
+    int quantidade;
+    int status;
+    system("clear");
+    printf("|--------------------- Cadastro de Livros --------------------|\n");
+    printf("\n");
+    printf("Quantos livros deseja cadastra: ");
+    scanf("%d", &quantidade);
+
+    for (int i = 0; i <= quantidade - 1; i++)
+    {
+        books[*totalLivros].Codigo = *totalLivros;
+        printf("Codigo do Livro: %d", *totalLivros);
+        printf("\nDigite o Titulo do Livro: ");
+        scanf("%s", books[*totalLivros].Titulo);
+        printf("Digite o Autor: ");
+        scanf("%s", books[*totalLivros].Autor);
+        printf("Digite a Editora: ");
+        scanf("%s", books[*totalLivros].Editora);
+        printf("Digite o Ano de Publicação: ");
+        scanf("%d", &books[*totalLivros].AnoPublicacao);
+        printf("Digite Quantidade Disponivel: ");
+        scanf("%d", &books[*totalLivros].QuantidadeDisponivel);
+        printf("Digite 0(disponivel) ou 1(indisponivel): ");
+        scanf("%d", &status);
+        books[*totalLivros].StatusEmprestimo = status;
+
+
+
+
+}
+void GetFirstBook(struct Book books[], int *totalLivros)
+{
+    int currentCodigo = 0;
+    printf("\n");
+    system("clear");
+    printf("|--------------------- Consultar Livro --------------------|\n");
+    printf("Qual o Codigo do livro que deseja procurar: ");
+    scanf("%d", &currentCodigo);
+
+    if (currentCodigo <= *totalLivros)
+    {
+        printf("\n");
+        printf("Codigo do Livro: %d\n", books[currentCodigo].Codigo);
+        printf("Titulo: %s\n", books[currentCodigo].Titulo);
+        printf("Autor: %s\n", books[currentCodigo].Autor);
+        printf("Editora: %s\n", books[currentCodigo].Editora);
+        printf("Ano de Publicação: %d\n", books[currentCodigo].AnoPublicacao);
+        printf("Unidades: %d\n", books[currentCodigo].QuantidadeDisponivel);
+        printf("Disponibilidade: %p\n", &books[currentCodigo].StatusEmprestimo);
+        system("clear");
+        printf("|------------------------------------------------------|\n");
+    }
+    else
+    {
+        printf("Livro Nao encontrado!!! \n");
+    }
+
+}
+void GetAllBooks(struct Book books[], int *totalLivros)
+{
+    system("clear");
+    printf("|--------------------- Relatorio de Livros --------------------|\n");
+    if (*totalLivros != 0)
+    {
+        for (int i =0; i <= *totalLivros -1; i++) {
+            printf("Codigo do livro:  %d\n", books[i].Codigo);
+            printf("Titulo do Livro:  %s\n", books[i].Titulo);
+            printf("Autor do Livro: %s\n", books[i].Autor);
+            printf("A Editora: %s\n", books[i].Editora);
+            printf("Ano de Publicação: %d\n", books[i].AnoPublicacao);
+            printf("Unidades: %d\n", books[i].QuantidadeDisponivel);
+            printf("Disponibilidade para Emprestimo: %p\n", &books[i].StatusEmprestimo);
+            printf("|-----------------------------------------------------|\n");
+        }
+    }else
+    {
+        printf("Nao temos livros Cadastrados ainda.\n");
+    }
+
+}
+
 //Menu Usuario
 void MenuUsuario()
 {
@@ -165,10 +268,9 @@ void MenuEmprestimo()
 }
 
 
+
 int main(void) {
-    struct Book books[100];
-    struct User users[100];
-    struct Loan loans[100];
+
 
     MenuPrincipal();
 
